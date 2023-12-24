7babed663a3b85b19ba270cf59919fe003aec2c9
khram2003 <nikita.khramov@gmail.com>
2023-12-24 13:48:50 +0100

    another approach

--- model/evaluate.py
+++ model/evaluate.py
@@ -3,6 +3,8 @@ from torch.utils.data import DataLoader
 from tqdm import tqdm
 from transformers import T5ForConditionalGeneration, AutoTokenizer
 from torchmetrics import BLEUScore
+from nltk.translate.bleu_score import corpus_bleu
+from sklearn.metrics import accuracy_score
 
 from dataset.dataset import MethodNameDataset
 
@@ -15,24 +17,28 @@ dataset = MethodNameDataset('../intellij-community')
 eval_loader = DataLoader(dataset, batch_size=1, shuffle=False)
 print(f'Number of samples: {len(dataset)}')
 
-
-def evaluate_pretrained_model():
-    model.eval()
-    bleu = BLEUScore()
-
-    with torch.no_grad():
-        for bodies, names in tqdm(eval_loader):
-            inputs = tokenizer.encode(bodies[0], return_tensors='pt').to(device)
-            if inputs[0].shape[0] > 512:
-                continue
-            outputs = model.generate(inputs, max_length=10)
-            predicted_name = tokenizer.decode(outputs[0], skip_special_tokens=True)
-            print(predicted_name)
-            bleu.update(predicted_name, names[0])
-
-    return bleu.compute()
+actual_names = []
+predicted_names = []
 
 
-if __name__ == '__main__':
-    bleu = evaluate_pretrained_model()
-    print(f'BLEU: {bleu}')
+def predict_method_name(body):
+    inputs = tokenizer.encode(body, return_tensors='pt').to(device)
+    outputs = model.generate(inputs, max_length=10)
+    predicted_name = tokenizer.decode(outputs[0], skip_special_tokens=True)
+    return predicted_name
+
+
+for method_body, actual_name in eval_loader:
+    predicted_name = predict_method_name(method_body[0])
+    actual_names.append([actual_name])
+    predicted_names.append(predicted_name)
+
+bleu_score = corpus_bleu(actual_names, predicted_names)
+
+flat_actual_names = [name for sublist in actual_names for name in sublist]
+flat_predicted_names = predicted_names
+
+accuracy = accuracy_score(flat_actual_names, flat_predicted_names)
+
+print(f"BLEU Score: {bleu_score}")
+print(f"Accuracy: {accuracy}")
